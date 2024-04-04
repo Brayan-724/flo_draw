32dc8ba97a9f07eed92c469cb36619c14cde3680
Andrew Hunter <andrew@logicalshift.co.uk>
2024-04-04 22:09:27 +0100

    Render the 'before' and 'after' scanlines as well as the main target scanline

--- render_software/src/pixel_programs/filtered_scanline.rs
+++ render_software/src/pixel_programs/filtered_scanline.rs
@@ -103,51 +103,68 @@ where
 
         // Try to retrieve the scanline, or plan a new one if needed
         // TODO: reset scanlines if the x-transform or the width has changed (maybe also if the render height is changed)
-        let scanline = {
-            let scanlines = data.scanlines.read().unwrap();
-            if let Some(existing_scanline) = scanlines.get(&y_pos.to_bits()) {
-                // Re-use the previously calculated scanline
-                Arc::clone(existing_scanline)
-            } else {
-                // Cache a new scanline to re-use
-                mem::drop(scanlines);
+        let render_scanlines = (-(before_y as i64)..=(after_y as i64))
+            .map(|y_offset| {
+                let scan_ypos = scan_ypos + (y_offset as f64);
+                let scanlines = data.scanlines.read().unwrap();
+                if let Some(existing_scanline) = scanlines.get(&scan_ypos.to_bits()) {
+                    // Re-use the previously calculated scanline
+                    Arc::clone(existing_scanline)
+                } else {
+                    // Cache a new scanline to re-use
+                    mem::drop(scanlines);
 
-                // Calculate the transform for the sprite region
-                let scan_xrange = scan_transform.pixel_x_to_source_x(-(before_x as i32))..scan_transform.pixel_x_to_source_x(x_transform.width_in_pixels() as i32 + after_x as i32);
+                    // Calculate the transform for the sprite region
+                    let scan_xrange = scan_transform.pixel_x_to_source_x(-(before_x as i32))..scan_transform.pixel_x_to_source_x(x_transform.width_in_pixels() as i32 + after_x as i32);
 
-                // Plan the rendering for the sprite
-                let mut new_scanline = [(scan_ypos, ScanlinePlan::default())];
-                self.planner.plan_scanlines(&*data.edges, &scan_transform, &[scan_ypos], scan_xrange, &mut new_scanline);
+                    // Plan the rendering for the sprite
+                    let mut new_scanline = [(scan_ypos, ScanlinePlan::default())];
+                    self.planner.plan_scanlines(&*data.edges, &scan_transform, &[scan_ypos], scan_xrange, &mut new_scanline);
 
-                // Store as the new cached value
-                let mut scanlines = data.scanlines.write().unwrap();
+                    // Store as the new cached value
+                    let mut scanlines = data.scanlines.write().unwrap();
 
-                let mut new_plan = ScanlinePlan::default();
-                mem::swap(&mut new_scanline[0].1, &mut new_plan);
-                let new_scanline = Arc::new(new_plan);
+                    let mut new_plan = ScanlinePlan::default();
+                    mem::swap(&mut new_scanline[0].1, &mut new_plan);
+                    let new_scanline = Arc::new(new_plan);
 
-                scanlines.insert(y_pos.to_bits(), new_scanline.clone());
+                    scanlines.insert(y_pos.to_bits(), new_scanline.clone());
 
-                new_scanline
-            }
-        };
+                    new_scanline
+                }
+            });
 
         // Clip the plan against the x-region that's being rendered (so we don't render any more pixels than we actually need)
-        let x_start     = pixel_range.start as f64 - before_x as f64;
-        let x_end       = pixel_range.end as f64 + after_x as f64;
-        let x_range     = x_start..x_end;
-        let scanline    = scanline.clip(x_range, x_start);
+        let x_start             = pixel_range.start as f64 - before_x as f64;
+        let x_end               = pixel_range.end as f64 + after_x as f64;
+        let x_range             = x_start..x_end;
+        let render_scanlines    = render_scanlines.map(|scanline| scanline.clip(x_range.clone(), x_start));
 
-        // Render the scanline into its own buffer
-        let region              = ScanlineRenderRegion { y_pos: scan_ypos, transform: scan_transform };
-        let mut scanline_buffer = vec![<TFilter as PixelFilter>::Pixel::default(); pixel_range.len() + (before_x + after_x)];
-        data_cache.render(&region, &scanline, &mut scanline_buffer);
+        // Render the scanlines into their own buffers
+        let rendered_scanlines = (-(before_y as i64)..=(after_y as i64)).zip(render_scanlines)
+            .map(|(y_offset, scanline)| {
+                let scan_ypos = scan_ypos + (y_offset as f64);
+
+                let region              = ScanlineRenderRegion { y_pos: scan_ypos, transform: scan_transform };
+                let mut scanline_buffer = vec![<TFilter as PixelFilter>::Pixel::default(); pixel_range.len() + (before_x + after_x)];
+                data_cache.render(&region, &scanline, &mut scanline_buffer);
+
+                scanline_buffer
+            })
+            .collect::<Vec<_>>();
+
+        let scanline_refs = rendered_scanlines.iter()
+            .map(|scanline| {
+                let scanline: &[TFilter::Pixel] = scanline;
+                scanline
+            })
+            .collect::<Vec<_>>();
 
         // Apply the filter to generate the final result
         let mut filter_result   = vec![<TFilter as PixelFilter>::Pixel::default(); pixel_range.len()];
         let filter_ypos         = scan_transform.source_x_to_pixels(scan_ypos) - scan_transform.source_x_to_pixels(-1.0);
 
-        data.filter.filter_line(filter_ypos as usize, &[&scanline_buffer], &mut filter_result);
+        data.filter.filter_line(filter_ypos as usize, &scanline_refs, &mut filter_result);
 
         for (src, tgt) in filter_result[0..pixel_range.len()].iter().zip(target[(pixel_range.start as usize)..(pixel_range.end as usize)].iter_mut()) {
             *tgt = src.source_over(*tgt);
